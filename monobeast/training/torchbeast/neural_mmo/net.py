09b15a3b569e2793dec11ffe30a8c86a83dbe9a2
haoyuan_jiang <27457-haoyuan_jiang@users.noreply.gitlab.aicrowd.com>
2022-04-28 09:10:25 +0800

    attack part

--- monobeast/training/torchbeast/neural_mmo/net.py
+++ monobeast/training/torchbeast/neural_mmo/net.py
@@ -1,6 +1,170 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+from torch.nn.init import xavier_normal_
+
+
+def sequential_pack(layers):
+    assert isinstance(layers, list)
+    seq = nn.Sequential(*layers)
+    for item in layers:
+        if isinstance(item, nn.Conv2d) or isinstance(item, nn.ConvTranspose2d):
+            seq.out_channels = item.out_channels
+            break
+        elif isinstance(item, nn.Conv1d):
+            seq.out_channels = item.out_channels
+            break
+    return seq
+
+
+def conv2d_block(
+    in_channels,
+    out_channels,
+    kernel_size,
+    stride=1,
+    padding=0,
+    dilation=1,
+    groups=1,
+    pad_type='zero',
+    activation=None,
+):
+    block = []
+    assert pad_type in ['zero', 'reflect', 'replication'], "invalid padding type: {}".format(pad_type)
+    if pad_type == 'zero':
+        pass
+    elif pad_type == 'reflect':
+        block.append(nn.ReflectionPad2d(padding))
+        padding = 0
+    elif pad_type == 'replication':
+        block.append(nn.ReplicationPad2d(padding))
+        padding = 0
+    block.append(
+        nn.Conv2d(in_channels, out_channels, kernel_size, stride, padding=padding, dilation=dilation, groups=groups)
+    )
+    xavier_normal_(block[-1].weight)
+    if activation is not None:
+        block.append(activation)
+    return sequential_pack(*block)
+
+
+class ResBlock(nn.Module):
+    def __init__(self, in_channels, middle_channels, activation=nn.ReLU(), res_type='basic'):
+        super(ResBlock, self).__init__()
+        self.act = activation
+        assert res_type in ['basic',
+                            'bottleneck'], 'residual type only support basic and bottleneck, not:{}'.format(res_type)
+        self.res_type = res_type
+
+        self.conv1 = conv2d_block(in_channels, middle_channels, 3, 1, 1, activation=self.act)
+        self.conv2 = conv2d_block(middle_channels, in_channels, 3, 1, 1, activation=None)
+
+    def forward(self, x):
+        residual = x
+        x = self.conv1(x)
+        x = self.conv2(x)
+        x = self.act(x + residual)
+        return x
+
+
+class SpatialEncoder(nn.Module):
+    def __init__(self, input_dim=56, project_dim=32):
+        super(SpatialEncoder, self).__init__()
+        self.act = nn.ReLU()
+        self.project = conv2d_block(input_dim, project_dim, 1, 1, 0, activation=self.act)
+        self.res = nn.ModuleList()
+        self.resblock_num = 2
+        for i in range(self.resblock_num):
+            self.res.append(ResBlock(project_dim, 2*project_dim, self.act))
+
+        # self.fc = nn.Linear( ,408)
+
+    def forward(self, x, scatter_map):
+        x = torch.cat([x, scatter_map], dim=1)
+        x = self.project(x)
+        for block in self.res:
+            x = block(x)
+        x = x.view(x.shape[0], -1)
+        x = self.fc(x)
+        return x
+
+
+class EntityEncoder(nn.Module):
+    r'''
+    B=batch size EN=any number of entities ID=input_dim OS=output_size=256
+     (B*EN*ID)  (EN'*OS)          (EN'*OS)          (EN'*OS)           (B*EN*OS)
+    x -> combine -> Transformer ->  act ->  entity_fc  -> split ->   entity_embeddings
+          batch                         |      (B*EN*OS)   (B*OS)        (B*OS)
+                                        \->  split ->  mean -> embed_fc -> embedded_entity
+    '''
+
+    def __init__(self, cfg):
+        super(EntityEncoder, self).__init__()
+        self.encode_modules = nn.ModuleDict()
+        self.whole_cfg = cfg
+        self.cfg = self.whole_cfg.model.encoder.obs_encoder.entity_encoder
+        for k, item in self.cfg.module.items():
+            if item['arc'] == 'one_hot':
+                self.encode_modules[k] = nn.Embedding.from_pretrained(torch.eye(item['num_embeddings']),freeze=True,padding_idx=None)
+            if item['arc'] == 'binary':
+                self.encode_modules[k] = torch.nn.Embedding.from_pretrained(get_binary_embed_mat(item['num_embeddings']),freeze=True,padding_idx=None)
+        self.act = build_activation(self.cfg.activation)
+        self.transformer = Transformer(
+            input_dim=self.cfg.input_dim,
+            head_dim=self.cfg.head_dim,
+            hidden_dim=self.cfg.hidden_dim,
+            output_dim=self.cfg.output_dim,
+            head_num=self.cfg.head_num,
+            mlp_num=self.cfg.mlp_num,
+            layer_num=self.cfg.layer_num,
+            dropout_ratio=self.cfg.dropout_ratio,
+            activation=self.act,
+            ln_type=self.cfg.ln_type
+        )
+        self.entity_fc = fc_block(self.cfg.output_dim, self.cfg.output_dim, activation=self.act)
+        self.embed_fc = fc_block(self.cfg.output_dim, self.cfg.output_dim, activation=self.act)
+        if self.whole_cfg.model.entity_reduce_type == 'attention_pool':
+            self.attention_pool = AttentionPool(key_dim=self.cfg.output_dim, head_num=2, output_dim=self.cfg.output_dim)
+        elif self.whole_cfg.model.entity_reduce_type == 'attention_pool_add_num':
+            self.attention_pool = AttentionPool(key_dim=self.cfg.output_dim, head_num=2, output_dim=self.cfg.output_dim, max_num=MAX_ENTITY_NUM + 1)
+
+    def forward(self, x: Dict[str, Tensor], entity_num):
+        entity_embedding = []
+        for k, item in self.cfg.module.items():
+            assert k in x.keys(), '{} not in {}'.format(k, x.keys())
+            if item['arc'] == 'one_hot':
+                # check data
+                over_cross_data = x[k] >= item['num_embeddings']
+                # if over_cross_data.any():
+                #     print(k, x[k][over_cross_data])
+                lower_cross_data = x[k] < 0
+                if lower_cross_data.any():
+                    print(k, x[k][lower_cross_data])
+                    raise RuntimeError
+                clipped_data = x[k].long().clamp_(max=item['num_embeddings'] - 1)
+                entity_embedding.append(self.encode_modules[k](clipped_data))
+            elif item['arc'] == 'binary':
+                entity_embedding.append(self.encode_modules[k](x[k].long()))
+            elif item['arc'] == 'unsqueeze':
+                entity_embedding.append(x[k].float().unsqueeze(dim=-1))
+        x = torch.cat(entity_embedding, dim=-1)
+        mask = sequence_mask(entity_num, max_len=x.shape[1])
+        x = self.transformer(x, mask=mask)
+        entity_embeddings = self.entity_fc(self.act(x))
+
+        if self.whole_cfg.model.entity_reduce_type in ['entity_num', 'selected_units_num']:
+            x_mask = x * mask.unsqueeze(dim=2)
+            embedded_entity = x_mask.sum(dim=1) / entity_num.unsqueeze(dim=-1)
+        elif self.whole_cfg.model.entity_reduce_type == 'constant':
+            x_mask = x * mask.unsqueeze(dim=2)
+            embedded_entity = x_mask.sum(dim=1) / 512
+        elif self.whole_cfg.model.entity_reduce_type == 'attention_pool':
+            embedded_entity = self.attention_pool(x, mask=mask.unsqueeze(dim=2))
+        elif self.whole_cfg.model.entity_reduce_type == 'attention_pool_add_num':
+            embedded_entity = self.attention_pool(x, num=entity_num, mask=mask.unsqueeze(dim=2))
+        else:
+            raise NotImplementedError
+        embedded_entity = self.embed_fc(embedded_entity)
+        return entity_embeddings, embedded_entity, mask
 
 
 class NMMONet(nn.Module):
@@ -14,22 +178,7 @@ class NMMONet(nn.Module):
         #     nn.Linear(1024, 512),
         #     nn.ReLU(),
         # )
-        self.cnn = nn.Sequential(
-            nn.Conv2d(in_channels=7,
-                      out_channels=16,
-                      kernel_size=3,
-                      stride=1,
-                      padding=1), nn.ReLU(),
-            nn.Conv2d(in_channels=16,
-                      out_channels=16,
-                      kernel_size=3,
-                      stride=1,
-                      padding=1), nn.ReLU(),
-            nn.Conv2d(in_channels=16,
-                      out_channels=8,
-                      kernel_size=3,
-                      stride=1,
-                      padding=1), nn.ReLU())
+        self.spatialEncoder = SpatialEncoder()
         self.core = nn.Linear(512 + 8 * 15 * 15, 512)
 
         self.unit_nn = nn.Linear(27, 48)
--- monobeast/training/torchbeast/neural_mmo/train_wrapper.py
+++ monobeast/training/torchbeast/neural_mmo/train_wrapper.py
@@ -3,30 +3,48 @@ import numpy as np
 from gym import Wrapper, spaces
 from ijcai2022nmmo import TeamBasedEnv
 from ijcai2022nmmo.scripted import CombatTeam, ForageTeam, RandomTeam
+from ijcai2022nmmo.scripted.baselines import Scripted
+from ijcai2022nmmo.scripted.scripted_team import ScriptedTeam
 
 
 class FeatureParser:  # 环境obs解析
+    map_size = 15
+    n_move_actions = 5  # 0不动 1上 2下 3左 4右
+    NEIGHBOR = [(6, 7), (8, 7), (7, 8), (7, 6)]  # north, south, east, west
+    OBSTACLE = (0, 1, 5) # lava, water, stone
+    feature_spec = {
+        "terrain": spaces.Box(low=0, high=6, shape=(15, 15), dtype=np.int64),
+        "camp": spaces.Box(low=0, high=4, shape=(15, 15), dtype=np.int64),
+        "entity": spaces.Box(low=0,
+                             high=4,
+                             shape=(7, 15, 15),
+                             dtype=np.float32),
+        "va": spaces.Box(low=0, high=2, shape=(5, ), dtype=np.int64),
+    }
+
     def __init__(self, feas_dim):
 
         self.map_size = feas_dim[0]
-        self.channel_num = 6  # im
-        self.onehot = np.eye(self.channel_num)
-        self.onehot_team = np.eye(16)
+        # self.channel_num = 6  # im
+        # self.onehot = np.eye(self.channel_num)  # todo move to model
+        self.onehot_team = np.eye(17)
+        self.onehot_index = np.eye(9)
+        self.now_time = 0
 
     def parse(self, obs):
 
         frame_list = {}
 
         for entity in obs.keys():
-
+            va_move = np.ones(self.n_move_actions, dtype=np.int64)
             obs_agents = obs[entity]["Entity"]["Continuous"]
-            now_time = max(obs_agents[:, 8])
+            self.now_time = max(max(obs_agents[:, 8]), self.now_time)
 
             # agents_frame = obs_agents.reshape(-1) / np.linalg.norm(
             #     obs_agents.reshape(-1))   #TODO normalize
 
             obs_map = obs[entity]["Tile"]["Continuous"]
-            local_map = np.zeros((self.channel_num, self.map_size, self.map_size),
+            local_map = np.zeros((self.map_size, self.map_size),
                                  dtype=np.float32)
             agent_map = np.zeros((1, self.map_size, self.map_size),
                                  dtype=np.float32)
@@ -35,98 +53,108 @@ class FeatureParser:  # 环境obs解析
             init_C = obs_map[0][3]  # 左上角loc
 
             for line in obs_map:
-                local_map[:, int(line[2] - init_R),
-                             int(line[3] - init_C)] = self.onehot[int(line[1])]
+                local_map[int(line[2] - init_R),
+                          int(line[3] - init_C)] = int(line[1])
                 if line[0] != 0:
                     agent_map[0][int(line[2] - init_R),
-                                 int(line[3] - init_C)] = 1
+                                 int(line[3] - init_C)] = line[0]
 
-            obs_emb = np.zeros((100, 27), dtype="float32")
+            obs_num_part = np.zeros((100, 45), dtype="float32")
             index = 0
+            entity_loc = []
+            entity_id = []
+            entity_in = []
+            attack_id = []
+            team_in = []
             while index < 100:
                 value = obs_agents[index]
                 if value[0] == 0:
                     break
-                entity_in = 0 if value[1] < 0 else 1
+
+                entity_id.append(value[1])
+
+                entity_in_ = -1 if value[1] < 0 else value[1] % 16 - 1
+                entity_in.append(entity_in_)
+
+                attack_id_ = -1 if value[2] < 0 else value[2] % 16 - 1
+                attack_id.append(attack_id_)
+
                 level_in = value[3] / 10
-                team_in = self.onehot_team[int(value[4])] if value[4] > 0 else np.zeros(16)
+
+                team_in_ = value[4] if value[4] > 0 else -1
+                team_in.append(team_in_)
+
                 r_in = (value[5] - 64) / 128
                 c_in = (value[6] - 64) / 128
-                dr_in = (r_in-init_R) / 10
-                dc_in = (c_in-init_C) / 10
-                alive_in = 0 if value[8] < now_time else 1
+
+                dr_in = (r_in-init_R)
+                dc_in = (c_in-init_C)
+                assert -8 < dc_in < 8 and -8 < dr_in < 8
+                entity_loc.append([dc_in, dc_in])
+
+                dc_in /= 10
+                dr_in /= 10
+                alive_in = 0 if value[8] < self.now_time else 1
                 food_in = value[9] / 10   # 最大与等级相等
                 water_in = value[10] / 10  # 最大与等级相等
                 health_in = value[11] / 10  # 最大与等级相等  todo Add relative value of integral
                 freezed_in = value[12]   # todo 加入目前freezed了多久
-                obs_emb[index, :] = np.hstack([entity_in, level_in, team_in,r_in, c_in, dr_in, dc_in,
-                                               alive_in,food_in, water_in, health_in, freezed_in])
+                obs_num_part[index, :] = np.hstack([level_in, r_in, c_in, dr_in, dc_in,
+                                                    alive_in, food_in, water_in, health_in,
+                                                    freezed_in])
 
                 index += 1
 
             mask = np.array([0 if i < index else 1 for i in range(100)], dtype="bool")
+            entity_loc = np.array(entity_loc, dtype='int')
+            entity_id = np.array(entity_id, dtype='int')
+            team_in = np.array(team_in, dtype='int')
+            attack_id = np.array(attack_id, dtype='int')
+            entity_in = np.array(entity_in, dtype='int')
 
             map_frame = np.concatenate([local_map, agent_map])
 
+            # valid action
+            for i, (r, c) in enumerate(self.NEIGHBOR):
+                if local_map[r, c] in self.OBSTACLE:
+                    va_move[i + 1] = 0
+
             frame_list[entity] = {
-                "obs_emb": obs_emb,
+                "obs_emb": obs_num_part,
                 "map_frame": map_frame,
-                "mask": mask
+                "mask": mask,
+                "entity_loc": entity_loc,
+                "entity_id": entity_id,
+                "team_in": team_in,
+                "attack_id": attack_id,
+                "entity_in": entity_in,
+                "va_move": va_move
             }
 
         return frame_list
 
 
 class RewardParser:
-    def parse(self, pre_obs, obs):
 
-        reward_list = {}
-
-        for entity in obs.keys():
-
-            pre_feas = pre_obs[entity]["Entity"]["Continuous"][0]
-            feas = obs[entity]["Entity"]["Continuous"][0]
-
-            reward = 0
-
-            # _, AgentID, _, last_level, _, last_R, last_C, last_damage, last_timealive, last_food, last_water, last_health, last_freeze = self_feas[entity]
-            # _, AgentID, _,      level, _,      R,      C,      damage,      timealive,      food,      water,      health,      freeze = feas[entity]
-
-            last_timealive = pre_feas[8]
-            timealive = feas[8]
-
-            if timealive > last_timealive:
-                reward = 1
-
-            reward_list[entity] = reward
-
-        return reward_list
+    def parse(self, prev_achv, achv):
+        reward = {
+            i: (sum(achv[i].values()) - sum(prev_achv[i].values())) / 100.0
+            for i in achv
+        }
+        return reward
 
 
 class TrainWrapper(Wrapper):
-    MAX_STEP = 1024
-    TRAINING_TEAM_IDX = 0
+    max_step = 1024
+    TT_ID = 0  # training team index
+    use_auxiliary_script = False
 
     def __init__(self, env: TeamBasedEnv) -> None:
         super().__init__(env)
         self.feature_parser = FeatureParser(feas_dim=[15])
         self.reward_parser = RewardParser()
-        self.observation_space = spaces.Dict({
-            "obs_emb":
-            spaces.Box(low=-10, high=255, shape=(100, 27), dtype=np.float32),
-            "map_frame":
-            spaces.Box(low=0, high=255, shape=(7, 15, 15), dtype=np.float32),
-            "mask":
-            spaces.Box(low=0, high=1, shape=(100, 1), dtype=np.bool),
-        })
-        self.action_space = spaces.Discrete(8)
-
-        self._scripted_random = RandomTeam(team_id="random",
-                                           env_config=env.config)
-        self._scripted_forage = ForageTeam(team_id="forage",
-                                           env_config=env.config)
-        self._scripted_combat = CombatTeam(team_id="combat",
-                                           env_config=env.config)
+        self.observation_space = spaces.Dict(self.feature_parser.feature_spec)
+        self.action_space = spaces.Discrete(5)
 
         self._dummy_feature = {
             key: np.zeros(shape=val.shape, dtype=val.dtype)
@@ -153,40 +181,35 @@ class TrainWrapper(Wrapper):
 
     def reset(self):
         raw_obs = super().reset()
-        obs = raw_obs[self.TRAINING_TEAM_IDX]  # select training team
+        obs = raw_obs[self.TT_ID]
         obs = self.feature_parser.parse(obs)
+
+        self.reset_auxiliary_script(self.config)
+        self.reset_scripted_team(self.config)
         self.agents = list(obs.keys())
-        self._prev_raw_obs = raw_obs  # 判断生存
+        self._prev_achv = self.metrices_by_team()[self.TT_ID]
+        self._prev_raw_obs = raw_obs
         self._step = 0
+
         return obs
 
     def step(self, actions):
-        scripted_decisions = self._scripted_action(self._prev_raw_obs)  # todo  联盟学习and self-play
-        decisions = {
-            self.TRAINING_TEAM_IDX:
-            self._parse_action(
-                actions,
-                alive_agents=list(
-                    self._prev_raw_obs[self.TRAINING_TEAM_IDX].keys()))
-        }
-        decisions.update(scripted_decisions)
+        decisions = self.get_scripted_team_decision(self._prev_raw_obs)  # todo  联盟学习and self-play
+        decisions[self.TT_ID] = self.transform_action(
+            actions,
+            observations=self._prev_raw_obs[self.TT_ID],
+            auxiliary_script=self.auxiliary_script)
 
-        raw_obs, raw_reward, raw_done, raw_info = super().step(decisions)
-        if self.TRAINING_TEAM_IDX in raw_obs:
-            obs = raw_obs[self.TRAINING_TEAM_IDX]
-            reward = raw_reward[self.TRAINING_TEAM_IDX]
-            done = raw_done[self.TRAINING_TEAM_IDX]
-            info = raw_info[self.TRAINING_TEAM_IDX]
+        raw_obs, _, raw_done, raw_info = super().step(decisions)
+        if self.TT_ID in raw_obs:
+            obs = raw_obs[self.TT_ID]
+            done = raw_done[self.TT_ID]
+            info = raw_info[self.TT_ID]
 
             obs = self.feature_parser.parse(obs)
-            reward = self.reward_parser.parse(
-                self._prev_raw_obs[self.TRAINING_TEAM_IDX],
-                raw_obs[self.TRAINING_TEAM_IDX])
-            # for k, v in reward_.items():
-            #     if k in reward:
-            #         reward[k] += v
-            #     else:
-            #         reward[k] = v
+            achv = self.metrices_by_team()[self.TT_ID]
+            reward = self.reward_parser.parse(self._prev_achv, achv)
+            self._prev_achv = achv
         else:
             obs, reward, done, info = {}, {}, {}, {}
 
@@ -199,64 +222,93 @@ class TrainWrapper(Wrapper):
         self._prev_raw_obs = raw_obs
         self._step += 1
 
-        if self._step >= self.MAX_STEP:
+        if self._step >= self.max_step:
             done = {key: True for key in done.keys()}
         return obs, reward, done, info
 
-    def _scripted_action(self, observations):
+    def reset_auxiliary_script(self, config):
+        if not self.use_auxiliary_script:
+            self.auxiliary_script = None
+            return
+        if getattr(self, "auxiliary_script", None) is not None:
+            self.auxiliary_script.reset()
+            return
+        self.auxiliary_script = AttackTeam("auxiliary", config)
+
+
+    def reset_scripted_team(self, config):
+        if getattr(self, "_scripted_team", None) is not None:
+            for team in self._scripted_team.values():
+                team.reset()
+            return
+        self._scripted_team = {}
+        assert config.NPOP == 16
+        for i in range(config.NPOP):
+            if i == self.TT_ID:
+                continue
+            if self.TT_ID < i <= self.TT_ID + 7:
+                self._scripted_team[i] = RandomTeam(f"random-{i}", config)
+            elif self.TT_ID + 7 < i <= self.TT_ID + 12:
+                self._scripted_team[i] = ForageTeam(f"forage-{i}", config)
+            elif self.TT_ID + 12 < i <= self.TT_ID + 15:
+                self._scripted_team[i] = CombatTeam(f"combat-{i}", config)
+
+    def get_scripted_team_decision(self, observations):
         decisions = {}
-        tt_id = self.TRAINING_TEAM_IDX
+        tt_id = self.TT_ID
         for team_id, obs in observations.items():
             if team_id == tt_id:
                 continue
-            if tt_id < team_id <= tt_id + 7:
-                decisions[team_id] = self._scripted_random.act(obs)
-
-            elif tt_id + 7 < team_id <= tt_id + 12:
-                decisions[team_id] = self._scripted_forage.act(obs)
-
-            elif tt_id + 12 < team_id <= tt_id + 15:
-                decisions[team_id] = self._scripted_combat.act(obs)
-
-            else:
-                raise ValueError(f"invalid team id: {team_id}")
+            decisions[team_id] = self._scripted_team[team_id].act(obs)
         return decisions
 
     @staticmethod
-    def _parse_action(actions, alive_agents=None):
-        """
-        decisions = {
-            action.Attack: {
-                action.Style: 0,
-                action.Target: 1
-            },
-            action.Move: {
-                action.Direction: 1
-            }
-        }
-        """
+    def transform_action(actions, observations=None, auxiliary_script=None):
+        """neural network move + scripted attack"""
         decisions = {}
-        for agent_id, act in actions.items():
-            if alive_agents is not None and agent_id not in alive_agents:
+
+        # move decisions
+        for agent_id, val in actions.items():
+            if observations is not None and agent_id not in observations:
                 continue
-            if act == 0:
+            if val == 0:
                 decisions[agent_id] = {}
-            elif 1 <= act <= 3:
-                decisions[agent_id] = {
-                    nmmo.action.Attack: {
-                        nmmo.action.Style: act - 1,
-                        nmmo.action.Target: 2
-                    }
-                }
-            elif 4 <= act <= 7:
+            elif 1 <= val <= 4:
                 decisions[agent_id] = {
                     nmmo.action.Move: {
-                        nmmo.action.Direction: act - 4
+                        nmmo.action.Direction: val - 1
                     }
                 }
             else:
-                raise ValueError(f"invalid action: {act}")
+                raise ValueError(f"invalid action: {val}")
+
+        # attack decisions
+        if auxiliary_script is not None:
+            assert observations is not None
+            attack_decisions = auxiliary_script.act(observations)
+            # merge decisions
+            for agent_id, d in decisions.items():
+                d.update(attack_decisions[agent_id])
+                decisions[agent_id] = d
         return decisions
+
+
+class Attack(Scripted):
+    '''attack'''
+    name = 'Attack_'
+
+    def __call__(self, obs):
+        super().__call__(obs)
+
+        self.scan_agents()
+        self.target_weak()
+        self.style = nmmo.action.Range
+        self.attack()
+        return self.actions
+
+
+class AttackTeam(ScriptedTeam):
+    agent_klass = Attack
 
 
 if __name__ == "__main__":
